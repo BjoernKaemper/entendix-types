08c441e1312f55298f68d9a4dd4f53d517064a34
BenediktEngel <b.engel@klickmeister.de>
2024-10-08 15:51:19 +0200

    feat: add submodule types

--- enums/SemanticSubmoduleTypes.ts
+++ enums/SemanticSubmoduleTypes.ts
@@ -8,4 +8,21 @@ export enum SemanticSubmoduleTypes {
   BUILDUNG_AUTOMATION_SYSTEM = 'Gebäudeautomationssystem',
   // Plants
   VENTILATION_PLANT = 'Lüftungsanlagen',
+  // Modules
+  PRE_HEATER = 'Vorerhitzer',
+  HEAT_RECOVERY = 'Wärmerückgewinnung',
+  COOLER = 'Kühler',
+  HUMIDIFIER = 'Befeuchter',
+  FAN_ASSEMBLY = 'Ventilatorbaugruppe',
+  AIR_DUCT = 'Luftkanal',
+  POST_HEATER = 'Nacherhitzer',
+  HEATER = 'Erhitzer',
+  FILTER_GROUP = 'Filtergruppe',
+  // Aggregates
+  PUMP = 'Pumpe',
+  VALVE = 'Ventil',
+  ROTARY_HEAT_EXCHANGER = 'Rotationswärmetauscher',
+  WATER_METER = 'Waserzähler',
+  SENSOR = 'Fühler',
+  VOLUME_FLOW_CONTROLLER = 'Volumenstromregler',
 }
