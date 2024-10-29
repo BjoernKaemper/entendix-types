8d25c25ed6d1a4440689b7434730d8589e3dd8b8
Nils Polarek <n.polarek@klickmeister.de>
2024-10-29 15:32:53 +0100

    extend aggregates types

--- aggregate/Aggregate.ts
+++ aggregate/Aggregate.ts
@@ -1,4 +1,14 @@
 import { ConditionTypes } from '../enums/ConditionTypes';
+/**
+ * Transformed information about a single BACnet datapoint
+ */
+export interface BACnetDatapoint {
+  description: string;
+  presentValue: string;
+  objectIdentifier: string;
+  objectName: string;
+  objectType: string;
+}
 
 /**
  * Aggregate interface
@@ -8,6 +18,7 @@ import { ConditionTypes } from '../enums/ConditionTypes';
  * @property {string} data.aggregateName - The name of the aggregate
  * @property {string} data.aggregateType - The type of the aggregate
  * @property {ConditionTypes} data.condition - The condition of the aggregate
+ * @property {Object} data.dataPoints - The data points of the aggregate
  */
 export interface Aggregate {
   id: string;
@@ -15,5 +26,20 @@ export interface Aggregate {
     aggregateName: string;
     aggregateType: string;
     condition?: ConditionTypes;
+    dataPoints?: {
+      [key: string]: {
+        presentValue: string;
+        description: {
+          de: string;
+          en: string;
+        };
+        unit: string;
+        dataSource: {
+          type: string;
+          value: string;
+        }[];
+        dataPoint?: BACnetDatapoint;
+      };
+    };
   };
 }
