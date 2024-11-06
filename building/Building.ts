e5ec54336551a73ef415cd5a97a708d8f65bf994
Nils Polarek <n.polarek@klickmeister.de>
2024-11-06 16:08:06 +0100

    add types for building add/update

--- building/Building.ts
+++ building/Building.ts
@@ -1,4 +1,4 @@
-import type { AddressWithGeoLatLong } from '../general/Address';
+import type { AddressWithGeoLatLong, Address } from '../general/Address';
 import type { BuildingSubsection } from './BuildingSubsection';
 
 /**
@@ -24,3 +24,18 @@ export interface Building {
     subsections?: Array<BuildingSubsection>;
   };
 }
+
+/**
+ * Flat object of changeable props for a building for adding/editing a building
+ */
+export interface FlatBuildingData extends Address {
+  buildingName: string;
+  usableSpace: number;
+  buildingVolume: number;
+  quantityFloors: number;
+}
+
+/**
+ * Partial FlatBuildingData object for updating a building
+ */
+export type BuildingUpdateData = Partial<FlatBuildingData>;
